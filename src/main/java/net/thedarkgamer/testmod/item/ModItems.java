3066af31c086842e695512a3558457b23825769a
Jonathan Morton <jonathan.x.morton@outlook.com>
2021-12-09 20:35:24 -0600

    Version 1.17.1-0.0.3: Add Ether Ingot Files

---
+++ src/main/java/net/thedarkgamer/testmod/item/ModItems.java
@@ -0,0 +1,19 @@
+package net.thedarkgamer.testmod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.thedarkgamer.testmod.TestMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Item> ETHER_INGOT = ITEMS.register("ether_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
