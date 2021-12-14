5d8e3b1ccdb101994fd5be370834c2e4e5f38fd8
Jonathan Morton <jonathan.x.morton@outlook.com>
2021-12-14 15:44:06 -0600

    Version 1.17.1-0.0.11 (15:44 Dec 14 2021): Add Golden Steak

--- src/main/java/net/thedarkgamer/testmod/item/ModItems.java
+++ src/main/java/net/thedarkgamer/testmod/item/ModItems.java
@@ -1,5 +1,6 @@
 package net.thedarkgamer.testmod.item;
 
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -11,10 +12,13 @@ import net.thedarkgamer.testmod.TestMod;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
 
+    //Ether
     public static final RegistryObject<Item> ETHER_INGOT = ITEMS.register("ether_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MAIN_TAB)));
     public static final RegistryObject<Item> ETHER_NUGGET = ITEMS.register("ether_nugget", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MAIN_TAB)));
     public static final RegistryObject<Item> RAW_ETHER = ITEMS.register("raw_ether", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MAIN_TAB)));
 
+    //Food
+    public static final RegistryObject<Item> GOLDEN_STEAK = ITEMS.register("golden_steak", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MAIN_TAB).food(new FoodProperties.Builder().meat().nutrition(10).saturationMod(0.5f).build())));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
