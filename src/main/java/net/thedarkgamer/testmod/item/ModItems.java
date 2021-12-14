56048f7cb53526078bbb516a939ecabfb06e8aef
Jonathan Morton <jonathan.x.morton@outlook.com>
2021-12-13 18:56:33 -0600

    Version 1.17.1-0.0.4: Add Raw Ether and Ether Nuggets

--- src/main/java/net/thedarkgamer/testmod/item/ModItems.java
+++ src/main/java/net/thedarkgamer/testmod/item/ModItems.java
@@ -12,6 +12,9 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
 
     public static final RegistryObject<Item> ETHER_INGOT = ITEMS.register("ether_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> ETHER_NUGGET = ITEMS.register("ether_nugget", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> RAW_ETHER = ITEMS.register("raw_ether", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
